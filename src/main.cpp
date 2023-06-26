9c42359db48658855289f61bd10b80a21ef18e4d
marquinho94 <marcoalanmiranda@gmail.com>
2023-06-26 17:49:25 -0300

    segunda parte

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include "main.h"
+
 #include <Arduino.h>
 #include <my_lib.h>
 //varaibles globales
@@ -6,14 +6,42 @@
 int a=3 , b=4, c=5;
 // put function declarations here:
 
+class animales {
+  public:
+  animales(){};
+
+  String nombre;
+  String apellido;
+  int edad;
+  void mostrarNota(){
+    Serial.println(notas);
+  }
+  void setAnimal(String nom, String ape, int ed, String notes){
+    nombre = nom;
+    apellido = ape;
+    edad = ed;
+    notas=notes;
+  };
+  int proxVacunacion(int algo){
+    return(algo*edad);
+  
+  };
+  private:
+  String notas;
+  protected:
+};
+
+animales perro;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  perro.setAnimal("Fatiga", "Argento", 5, "hola, soy una nota");
+  
 }
 
 void loop() {
-  int aux = suma(a,b);
+/*   int aux = suma(a,b);
   Serial.print("suma");
   Serial.println(aux);
   int aux2 = resta(a,b);
@@ -21,24 +49,15 @@ void loop() {
   Serial.println(aux2);
   int aux3 = multip(a,b);
   Serial.print("multiplicacion");
-  Serial.println(aux3);
+  Serial.println(aux3); */
+  Serial.println(perro.nombre);
+  Serial.println(perro.edad);
+  perro.mostrarNota();
 
-  switch (expression)
-  {
-  case /* constant-expression */:
 
-  if (comparacion())
-  {
-    estado=siguiente_estado;
-  }else {estado= mismo_estado;}
-  
-    // comparacion()? estado=siguiente_estado : estado= mismo_estado;
-  
-    break;
-  
-  default:
-    break;
-  }
+
+
+ 
 
 
 }
